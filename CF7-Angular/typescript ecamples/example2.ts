e6a7a6c71103201a9e4fb35f5c672f47a92bfcc8
Alexandros.Zournatzis <57686529+alekosdaz@users.noreply.github.com>
2025-04-27 16:54:14 +0300

    Delete CF7-Angular directory

--- CF7-Angular/typescript ecamples/example2.ts
+++
@@ -1,4 +0,0 @@
-const personDescription1 = (name: String, city: String, age: number) =>
-    `${name} lives in ${city}, his age is ${age}. In 10 years he'll be ${age+10}`;
-  
-  console.log(personDescription1("Kostas", "Athens", 20));
